82aff27d6b9ec224d2834b34a9424c16c6ed6204
Dex Fitch <fitch.dexter@gmail.com>
2019-06-11 15:06:03 -0700

    I WANA LERN

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,19 @@
-class Student 
+class Student < User
+  attr_accessor :knowledge
+
+  @@all = []
+
+  def initialize
+    @knowledge = []
+    @@all << self
+  end
+
+  def learn(knowledge)
+    @knowledge << knowledge
+  end
+
+  def self.all 
+    @@all 
+  end
 
 end
